73f8af7388953eea320e8124a92c67ba8e0ac61e
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-21 15:59:08 +0100

    aggiunto bonus

--- main.js
+++ main.js
@@ -23,19 +23,11 @@ var app = new Vue(
         emailGenerator(){
             
             for(i=0; i<10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((res) => {
-                  //console.log(res.data.response);
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
                   let randomMail = res.data.response;
-                  this.mailArray.push(randomMail);
-                  
- 
+                  this.mailArray.push(randomMail);                 
                 })
-                
             }
-            console.log(this.mailArray);
-
-
         }
 
         }
