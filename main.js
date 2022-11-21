7ec2bff80543a6a6d706acfee290a6d7bab8cea6
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-21 15:55:24 +0100

    esercizio completo

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
-var app = new Vue({
-el: '#root',
-data: {
+var app = new Vue(
+    {
 
-},
-mounted() {
+    el: '#root',
 
-},
-beforeUpdate() {
+    data: {
 
-},
-methods: {
+           mailArray: [],
 
-}
-})
+        },
+
+    mounted() {
+
+        },
+
+    beforeUpdate() {
+
+        },
+
+    methods: {
+
+        
+        emailGenerator(){
+            
+            for(i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                  //console.log(res.data.response);
+                  let randomMail = res.data.response;
+                  this.mailArray.push(randomMail);
+                  
+ 
+                })
+                
+            }
+            console.log(this.mailArray);
+
+
+        }
+
+        }
+    }
+)
+
+
